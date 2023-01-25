08e2820b7c19c5d00267885d0eac4b8203b0a86a
nycire08 <nycirevaughn@gmail.com>
2023-01-25 18:59:55 -0500

    My work to start assignment 1.

---
+++ Large Scale Programming/src/org/howard/edu/lsp/hw1/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * Name: Nycire Vaughn
+ */
+
+package org.howard.edu.lsp.hw1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+
+	}
+
+}
